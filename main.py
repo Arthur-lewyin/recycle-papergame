ad46226235b6589b9ce891055b8c4fcaeaf17e73
Arthur-lewyin <159451233+Arthur-lewyin@users.noreply.github.com>
2024-09-11 17:44:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -42,12 +42,76 @@ def update():
     if len(items)==0:
         items = make_items(current_level)
 
-def make_items():
-    pass 
 #Make items
 #1.get the options from ITEMS list - random
 #2.Create actors and add it to items list
 #3.Layout items - display them with equal spacing
 #4.Animations - move the objects down
+
+def make_items(number_of_extra_items):
+    items_to_create = get_option_to_create(number_of_extra_items)
+    new_items = create_items(items_to_create)
+    layout_items(new_items)
+    animate_items(new_items)
+    return new_items
+
+def get_option_to_create(number_of_extra_items):
+    items_to_create = ["paper"]
+    for i in range(0,number_of_extra_items):
+        random_option = random.choice(ITEMS)
+        items_to_create.append(random_option)
+    return items_to_create
+
+
+def create_items(items_to_create):
+    new_items = []
+    for i in items_to_create:
+        item = Actor(i+'img')
+        new_items.append(item)
+    return new_items
+
+def layout_items(items_to_layout):
+    number_of_gaps = len(items_to_layout)+1
+    gaps_size = WIDTH/number_of_gaps
+    random.shuffle(items_to_layout)
+    for i, item in enumerate(items_to_layout):
+        new_x_pos=(i + 1) * gaps_size
+        item.x =new_x_pos
+    
+
+def animate_items(items_to_animate):
+     global animations
+     for i in items_to_animate:
+         duration = START_SPEED-current_level
+         animation = animate(i,duration=duration,on_finished=handle_game_over, y=HEIGHT)
+         animations.append(animation)
+
+def handle_game_over():
+    global game_over
+    game_over = True
+
+def on_mouse_down(pos):
+    global items
+    for i in items:
+        if i.collidepoint(pos):
+            if "paper" in i.image:
+                handle_game_complete()
+            else:
+                handle_game_over()
+
+def handle_game_complete():
+    global current_level,items,animations,game_complete
+    stop_animation(animations)
+    if current_level == FINAL_LEVEL:
+        game_complete = True
+    else:
+        current_level = current_level + 1
+        items=[]
+        animations = []
+
+def stop_animation(animations_to_stop):
+    for i in animations_to_stop:
+        if i.running:
+            i.stop()
 
 pgzrun.go()
